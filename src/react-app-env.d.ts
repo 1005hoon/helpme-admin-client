c9a825afcbb68779b4e977f6f4d38f70e6420c42
hoon <hoonnotes@naver.com>
2022-03-17 20:20:07 +0900

    chore: 환경변수 설정

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+    interface ProcessEnv {
+        readonly NODE_ENV: 'development' | 'production' | 'stage';
+        readonly SERVER_URL: string;
+        readonly REACT_APP_GOOGLE_OAUTH_CLIENT_ID: string;
+        readonly REACT_APP_GOOGLE_OAUTH_CLIENT_SECRET: string;
+    }
+}
